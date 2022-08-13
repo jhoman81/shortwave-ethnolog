ef9eea872e297c562635afb74728f557075b5051
jhoman81 <josh.homan@gmail.com>
2022-08-12 21:24:58 -0500

    standardized variables, updated place functions

--- hflog.py
+++ hflog.py
@@ -2,20 +2,18 @@
 Shortwave Ethnographic Logging Tool
 Author: Joshua Homan
 
-TODO: Convert save_data() function to write to SQL database
 TODO: Convert GUI to wxPython or other API
 TODO: Implement timer function for each broadcast/call
-TODO: Geo-code placenames
 TODO: Implement JSON parsing of language file
-TODO: Integrate kinship and social relationship information somehow -- autocomplete on speaker based on kinship database
 TODO: Record line-in audio and link to DB entry for later analysis
 
 """
 
+from pathlib import Path
 from tkinter import *
 import tkinter.messagebox
 from datetime import datetime
-import json
+import json, csv
 
 class Place:
 	def __init__(self, name, latitude, longitude):
@@ -23,8 +21,15 @@ class Place:
 		self.latitude = latitude
 		self.longitude = longitude
 
+	def __repr__(self):
+		return self.name + " is located at the coordinates " + str(self.latitude) + ", " + str(self.longitude)
+
+class Participant:
+	def __init__(self, name):
+		self.name = name
+
 class Call:
-	def __init__(self, place_one, place_two, participant_one, participant_two, topic, time, date):
+	def __init__(self, place_one, place_two, participant_one, participant_two, topic, time, date, note):
 		self.place_one = place_one
 		self.place_two = place_two
 		self.participant_one = participant_one
@@ -32,42 +37,61 @@ class Call:
 		self.topic = topic
 		self.time = time
 		self.date = datetime(date)
+		self.note = note
 
 def read_places(file):
-    places = []
-    with open(file, 'r') as place_file:
-        for line in place_file:
-            places.append(line.rstrip())
-    return places
+	filename = Path("./places.csv")
+	places = []
+	try:
+		with open(filename, 'r', encoding='UTF8') as place_file:
+			for line in place_file:
+				places.append(line.rstrip())
+	except EnvironmentError:
+		print("File does not exist.")
+	return places
 
-## Write data to textfile 
+def add_place(name, latitude, longitude):
+	header = ["Name", "Latitude", "Longitude"]
+	data = [name, latitude, longitude]
+	filename = Path("./places.csv")
+	if filename.is_file():
+		with open(filename, 'a', encoding='UTF8') as place_file:
+			writer = csv.writer(place_file)
+			writer.writerow(data)
+	else:
+		with open(filename, 'w', encoding='UTF8') as place_file:
+			writer = csv.writer(place_file)
+			writer.writerow(header)
+			writer.writerow(data)
 
 def save_data():
-	try:
-		fileD = open("swlog.txt", "a")
-		fileD.write("Time and Date: ")
-		fileD.write(str(datetime.now()))
-		fileD.write("\n")
-		fileD.write("Frequency: ")
-		fileD.write("%s " % frequency.get())
-		fileD.write("%s\n" % band.get())
-		fileD.write("\nSpeaker One: %s\n" % speakerOne.get())
-		fileD.write("Speaker One's Location: %s\n" % speakerOnePlace.get())
-		fileD.write("\nSpeaker Two: %s\n" % speakerTwo.get())
-		fileD.write("Speaker Two's Location: %s\n\n" % speakerTwoPlace.get())
-		fileD.write("Language Used: %s\n\n" % language.get())
-		fileD.write("Notes: %s\n\n" % notes.get("1.0", END))
-		fileD.write("-=-" * 20)
-		fileD.write("\n\n")
-		band.set(None)
-		speaker_one_place.set(None)
-		speakerTwoPlace.set(None)
-		frequency.delete(0, END)
-		speakerOne.delete(0, END)
-		speakerTwo.delete(0, END)
-		language.set(None)
-		notes.delete("1.0", END)
-	except Exception as ex:
+	filename = Path("./swlog.md")
+	if filename.is_file():
+		with open(filename, 'a') as log_file:
+			log_file.write("## New Entry")
+			log_file.write("Time and Date: ")
+			log_file.write(str(datetime.now()))
+			log_file.write("\n")
+			log_file.write("Frequency: ")
+			log_file.write("%s " % frequency.get())
+			log_file.write("%s\n" % band.get())
+			log_file.write("\nSpeaker One: %s\n" % speaker_one.get())
+			log_file.write("Speaker One's Location: %s\n" % speaker_onePlace.get())
+			log_file.write("\nSpeaker Two: %s\n" % speaker_two.get())
+			log_file.write("Speaker Two's Location: %s\n\n" % speaker_twoPlace.get())
+			log_file.write("Language Used: %s\n\n" % language.get())
+			log_file.write("Notes: %s\n\n" % notes.get("1.0", END))
+			log_file.write("-=-" * 20)
+			log_file.write("\n\n")
+			band.set(None)
+			speaker_one_place.set(None)
+			speaker_one_place.set(None)
+			frequency.delete(0, END)
+			speaker_one.delete(0, END)
+			speaker_two.delete(0, END)
+			language.set(None)
+			notes.delete("1.0", END)
+	else:
 		tkinter.messagebox.showerror("Error!", "Can't write to the file\n %s" % ex)
 
 ## Construct GUI -- to convert to wxPython or other GUI
@@ -87,8 +111,8 @@ Radiobutton(app, variable = band, text = "USB", value = "USB").pack()
 Radiobutton(app, variable = band, text = "LSB", value = "LSB").pack()
 
 Label(app, text = "Speaker One").pack()
-speakerOne = Entry(app)
-speakerOne.pack()
+speaker_one = Entry(app)
+speaker_one.pack()
 
 Label(app, text = "Speaker One's Location").pack()
 speaker_one_place = StringVar()
@@ -99,14 +123,14 @@ dropdown_one['width'] = 10
 dropdown_one.pack()
 
 Label(app, text = "Speaker Two").pack()
-speakerTwo = Entry(app)
-speakerTwo.pack()
+speaker_two = Entry(app)
+speaker_two.pack()
 
 Label(app, text = "Speaker Two's Location").pack()
-speakerTwoPlace = StringVar()
-speakerTwoPlace.set(None)
-optionsTwo = read_places("./places.txt")
-dropdown_two = OptionMenu(app, speakerTwoPlace, optionsTwo)
+speaker_twoPlace = StringVar()
+speaker_twoPlace.set(None)
+options_two = read_places("./places.txt")
+dropdown_two = OptionMenu(app, speaker_twoPlace, options_two)
 dropdown_two['width'] = 10
 dropdown_two.pack()
 
